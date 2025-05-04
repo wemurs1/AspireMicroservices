cc21a7c9baa405de9c98375da14f9210f6dd015b
Stuart Murray <stuart.murray.dev@outlook.com>
2025-05-04 16:04:14 +0100

    Add keycloak auth engine and secure basket

--- eshop-distributed/aspire/AppHost/Program.cs
+++ eshop-distributed/aspire/AppHost/Program.cs
@@ -5,10 +5,22 @@ var postgres = builder.AddPostgres("postgres").WithPgAdmin().WithDataVolume().Wi
 var catalogDb = postgres.AddDatabase("catalogDb");
 var cache = builder.AddRedis("cache").WithRedisInsight().WithDataVolume().WithLifetime(ContainerLifetime.Persistent);
 var rabbitMq = builder.AddRabbitMQ("rabbitmq").WithManagementPlugin().WithDataVolume().WithLifetime(ContainerLifetime.Persistent);
+var keyCloak = builder.AddKeycloak("keycloak", 8080).WithDataVolume().WithLifetime(ContainerLifetime.Persistent);
 
 // projects
-var catalog = builder.AddProject<Projects.Catalog>("catalog").WithReference(catalogDb).WithReference(rabbitMq).WaitFor(catalogDb).WaitFor(rabbitMq);
+var catalog = builder.AddProject<Projects.Catalog>("catalog")
+    .WithReference(catalogDb)
+    .WithReference(rabbitMq)
+    .WaitFor(catalogDb)
+    .WaitFor(rabbitMq);
 
-var basket = builder.AddProject<Projects.Basket>("basket").WithReference(cache).WithReference(catalog).WithReference(rabbitMq).WaitFor(cache).WaitFor(rabbitMq);
+var basket = builder.AddProject<Projects.Basket>("basket")
+    .WithReference(cache)
+    .WithReference(catalog)
+    .WithReference(rabbitMq)
+    .WithReference(keyCloak)
+    .WaitFor(cache)
+    .WaitFor(rabbitMq)
+    .WaitFor(keyCloak);
 
 builder.Build().Run();
--- eshop-distributed/services/Basket/Endpoints/BasketEndpoints.cs
+++ eshop-distributed/services/Basket/Endpoints/BasketEndpoints.cs
@@ -15,7 +15,8 @@ public static class BasketEndpoints
         })
         .WithName("GetBasket")
         .Produces<ShoppingCart>(StatusCodes.Status200OK)
-        .Produces(StatusCodes.Status404NotFound);
+        .Produces(StatusCodes.Status404NotFound)
+        .RequireAuthorization();
 
         group.MapPost("/", async (ShoppingCart shoppingCart, BasketServices service) =>
         {
@@ -23,7 +24,8 @@ public static class BasketEndpoints
             return Results.Created("GetBasket", shoppingCart);
         })
         .WithName("UpdateBasket")
-        .Produces<ShoppingCart>(StatusCodes.Status201Created);
+        .Produces<ShoppingCart>(StatusCodes.Status201Created)
+        .RequireAuthorization();
 
         group.MapDelete("/{userName}", async (string userName, BasketServices service) =>
         {
@@ -31,6 +33,7 @@ public static class BasketEndpoints
             return Results.NoContent();
         })
         .WithName("DeleteBasket")
-        .Produces(StatusCodes.Status204NoContent);
+        .Produces(StatusCodes.Status204NoContent)
+        .RequireAuthorization();
     }
 }
